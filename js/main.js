dcec20e9a69fc827e57c185c5f4c50673a286b1c
SneatX <santi02005@hotmail.com>
2024-04-18 16:40:58 -0500

    feat: :tada: Inicio repo

---
+++ js/main.js
@@ -0,0 +1,7 @@
+//para abrir mi package.json usamos "npm init"
+//Instalamos async-prompt para poder pedir inputs desde consola
+//node_modules se crea solo al descargar otras vainas que se me olvido el nombre
+
+import prompt  from "async-prompt"
+import {sumarRango} from "./modules/sumarRangoNumeros.js"
+
